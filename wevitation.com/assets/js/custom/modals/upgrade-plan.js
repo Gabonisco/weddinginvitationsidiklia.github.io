385fc79d73530009e0d8c972c4de16135e04a319
Gabonisco <143951553+Gabonisco@users.noreply.github.com>
2023-09-03 22:17:58 +0700

    Add files via upload

---
+++ wevitation.com/assets/js/custom/modals/upgrade-plan.js
@@ -0,0 +1,7 @@
+(()=>{"use strict";var __webpack_exports__={};/*!*****************************************************!*\
+!*** ../demo6/src/js/custom/modals/upgrade-plan.js ***!
+\*****************************************************/var KTModalUpgradePlan=function(){var modal;var planPeriodMonthButton;var planPeriodAnnualButton;var changePlanPrices=function(type){var items=[].slice.call(modal.querySelectorAll('[data-kt-plan-price-month]'));items.map(function(item){var monthPrice=item.getAttribute('data-kt-plan-price-month');var annualPrice=item.getAttribute('data-kt-plan-price-annual');if(type==='month'){item.innerHTML=monthPrice;}else if(type==='annual'){item.innerHTML=annualPrice;}});}
+var handlePlanPeriodSelection=function(){planPeriodMonthButton.addEventListener('click',function(e){changePlanPrices('month');});planPeriodAnnualButton.addEventListener('click',function(e){changePlanPrices('annual');});}
+var handleTabs=function(){KTUtil.on(modal,'[data-bs-toggle="tab"]','click',function(e){this.querySelector('[type="radio"]').checked=true;});}
+return{init:function(){modal=document.querySelector('#kt_modal_upgrade_plan');if(!modal){return;}
+planPeriodMonthButton=modal.querySelector('[data-kt-plan="month"]');planPeriodAnnualButton=modal.querySelector('[data-kt-plan="annual"]');handlePlanPeriodSelection();handleTabs();}}}();KTUtil.onDOMContentLoaded(function(){KTModalUpgradePlan.init();});})();
